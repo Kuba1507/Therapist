59a3f82a7c0158edd6136da444db4514a11ac908
Kuba1507 <kuba.romanski@onet.com.pl>
2023-12-05 08:31:57 +0100

    styled header for tablets

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,9 @@
+const body = document.querySelector("body");
 const burgerBtn = document.querySelector(".hamburger");
 const mobileNav = document.querySelector(".nav-mobile");
 
 const handleNav = () => {
+	body.classList.toggle("disable-scroll");
 	burgerBtn.classList.toggle("is-active");
 	mobileNav.classList.toggle("show-mobile");
 };
