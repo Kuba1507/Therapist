a460df01ab48ef0ef6cb01c06fdef6c55007def8
Kuba1507 <kuba.romanski@onet.com.pl>
2023-12-05 07:59:36 +0100

    scripted mobile nav

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,9 @@
-const burgerBtn = document.querySelector("hamburger");
-const mobileNav = document.querySelector("nav-mobile");
+const burgerBtn = document.querySelector(".hamburger");
+const mobileNav = document.querySelector(".nav-mobile");
 
 const handleNav = () => {
-    
-}
+	burgerBtn.classList.toggle("is-active");
+	mobileNav.classList.toggle("show-mobile");
+};
+
+burgerBtn.addEventListener("click", handleNav);
