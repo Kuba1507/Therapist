1843edc9b6c25e37be4e90593f982412b7234f29
Kuba1507 <kuba.romanski@onet.com.pl>
2023-12-05 09:11:12 +0100

    styling header for desktp

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 const body = document.querySelector("body");
 const burgerBtn = document.querySelector(".hamburger");
 const mobileNav = document.querySelector(".nav-mobile");
+const headerImg = document.querySelector(".header__image-img");
 
 const handleNav = () => {
 	body.classList.toggle("disable-scroll");
@@ -8,4 +9,14 @@ const handleNav = () => {
 	mobileNav.classList.toggle("show-mobile");
 };
 
+const changeImage = () => {
+	if (window.innerWidth >= 1050) {
+		headerImg.src = "dist/img/header-img.jpg";
+	} else {
+		headerImg.src = "dist/img/header-img-mobile.jpg";
+	}
+};
+
 burgerBtn.addEventListener("click", handleNav);
+window.onload = changeImage;
+window.onresize = changeImage;
